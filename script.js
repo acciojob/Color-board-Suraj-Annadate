90f93eeee9c3885159ace4d9e984490156e2c4ee
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-05-25 14:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+    var container = document.querySelector('.container');
+    
+    for (var i = 0; i < 800; i++) {
+      var square = document.createElement('div');
+      square.classList.add('square');
+      container.appendChild(square);
+    }
